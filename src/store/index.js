6d10623f4aa8abc581cc1e4c9cb7407178b3e789
abrunello <brunello@bludata.com>
2022-01-19 13:45:04 +0100

    dividing slices in multiple files

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterSliceReducer from './counter';
 import authSliceReducer from './auth';
 
-
-
 const store = configureStore({
   reducer: {
     counter: counterSliceReducer,
