d12c07d377798ef2124d9fad81f4c74cdb582005
abrunello <brunello@bludata.com>
2022-01-19 11:10:25 +0100

    using react useDispatch to update redux store state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,30 @@
-import { useSelector } from 'react-redux';
-import classes from './Counter.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {
-    
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    dispatch({
+      type: "+",
+    });
   };
-  const counter = useSelector(state => state.counter)
+  const decrementHandler = () => {
+    dispatch({
+      type: "-",
+    });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- {counter} --</div>
+      <div className={classes.value}> {counter} </div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
